9394b5c0a8e0faf84e1c29fa0dbc382f9093241d
Alex <chapp@umich.edu>
2019-04-21 16:55:53 -0400

    Gets rid of attempt to get the full document

--- main.js
+++ main.js
@@ -7,8 +7,6 @@ define(function (require) {
 
     var CommandManager = brackets.getModule("command/CommandManager"),
         KeyBindingManager = brackets.getModule("command/KeyBindingManager"),
-        DocumentManager = brackets.getModule("document/DocumentManager"),
-        FileSystem = brackets.getModule("filesystem/FileSystem"),
         EditorManager = brackets.getModule("editor/EditorManager");
 
     
@@ -16,32 +14,14 @@ define(function (require) {
     function handleCtrlClick() {
         var editor = EditorManager.getCurrentFullEditor();
         var selection = editor.getSelectedText();
-        window.alert("User has highlighted " + selection);
-        console.log("index.html");
         var filename = require.toUrl(selection);
-        console.log("ToURL is " + filename);
-        var file = FileSystem.getFileForPath(filename);
-        console.log(file);
-        DocumentManager.getDocumentForPath(filename, file)
-            .then(function (value) {
-                console.log("Value is " + value);
-            });
-        /*document.then = function (value) {
-            console.log("We get here");
-            console.log("Value is " + value);
-            //EditorManager.openDocument(document);  
-            //return;
-        };*/
-        console.log("WTF");
-    }
-    
-    
-    
+        window.alert("The full path to the file is: " + filename);
+    }    
     
     // First, register a command - a UI-less object associating an id to a handler
     var MY_COMMAND_ID = "ctrlClick.alertUs";   // package-style naming to avoid collisions
     CommandManager.register("CtrlClickFile", MY_COMMAND_ID, handleCtrlClick);
 
-    KeyBindingManager.addBinding(MY_COMMAND_ID, "Ctrl-Shift-Alt-B");
+    KeyBindingManager.addBinding(MY_COMMAND_ID, "Ctrl-P");
     
 });
