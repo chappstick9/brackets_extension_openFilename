f1045b8f8adb730fa44eb9f36f5e554112dd8382
John Krieg <jpkrieg@umich.edu>
2019-04-21 17:23:05 -0400

    oh my god

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
 /*global define, brackets*/
 
-/** Simple extension that adds a "File > Hello World" menu item */
 define(function (require) {
     "use strict";
 
